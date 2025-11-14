3784bece4f9be74020462d591b6cb641d9fab5ce
elbrunis <brunoiniestabarrera@gmail.com>
2025-11-14 10:41:51 +0100

    he pasado a formato export.c

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -3,47 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maximo <maximo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: runo <runo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 18:46:21 by mwilline          #+#    #+#             */
-/*   Updated: 2025/10/29 00:27:05 by maximo           ###   ########.fr       */
+/*   Updated: 2025/11/14 10:41:24 by runo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int builtin_export(char **args, t_env **env)
+static int	env_display(t_env **env)
 {
-	int i;
+	t_env	*tmp;
+
+	tmp = *env;
+	while (tmp)
+	{
+		if (tmp->value)
+			printf("declare -x %s=\"%s\"\n", tmp->key, tmp->value);
+		else
+			printf("declare -x %s\n", tmp->key);
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+int	builtin_export(char **args, t_env **env)
+{
+	int		i;
+	char	*sep;
+	char	*key;
+	char	*value;
 
 	i = 1;
-	if(!args[i])
-	{
-		t_env *tmp = *env;
-		while(tmp)
-		{
-			if(tmp->value)
-				printf("declare -x %s=\"%s\"\n", tmp->key, tmp->value);
-			else
-				printf("declare -x %s\n", tmp->key);
-			tmp = tmp->next;
-		}
-		return(0);
-	}
+	if (!args[i])
+		return (env_display(env));
 	while (args[i])
-    {
-        char *sep = ft_strchr(args[i], '=');
-        if (sep)
-        {
-            char *key = ft_substr(args[i], 0, sep - args[i]);
-            char *value = ft_strdup(sep + 1);
-            set_env_value(env, key, value);
-            free(key);
-            free(value);
-        }
-        else
-            set_env_value(env, args[i], NULL);
-        i++;
+	{
+		sep = ft_strchr(args[i], '=');
+		if (sep)
+		{
+			key = ft_substr(args[i], 0, sep - args[i]);
+			value = ft_strdup(sep + 1);
+			set_env_value(env, key, value);
+			free(key);
+			free(value);
+		}
+		else
+			set_env_value(env, args[i], NULL);
+		i++;
 	}
-    return (0);
+	return (0);
 }
+
